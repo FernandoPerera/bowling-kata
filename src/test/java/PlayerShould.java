8a05d5b0d5f287d9b628b3936f3c4b6710fdb202
FernandoPerera <fernando02perera@gmail.com>
2024-12-31 16:50:19 +0000

    :test_tube: not_update_the_score_before_the_end_of_the_turnover

--- src/test/java/PlayerShould.java
+++ src/test/java/PlayerShould.java
@@ -15,4 +15,14 @@ class PlayerShould {
         assertEquals(expectedScore, player.score());
     }
 
+    @Test
+    void not_update_the_score_before_the_end_of_the_turnover() {
+        Player player = new Player();
+        int expectedScore = 0;
+
+        player.roll(3);
+
+        assertEquals(expectedScore, player.score());
+    }
+
 }
