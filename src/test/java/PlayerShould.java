0c774da455c051d1a8b0854d611fb94549618b9e
FernandoPerera <fernando02perera@gmail.com>
2024-12-31 17:38:41 +0000

    :test_tube: not_update_the_score_when_it_achieves_a_spare

--- src/test/java/PlayerShould.java
+++ src/test/java/PlayerShould.java
@@ -1,12 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlayerShould {
 
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player();
+    }
+
     @Test
     void get_the_score_after_the_end_of_the_turnover() {
-        Player player = new Player();
         int expectedScore = 7;
 
         player.roll(3);
@@ -17,10 +24,19 @@ class PlayerShould {
 
     @Test
     void not_update_the_score_before_the_end_of_the_turnover() {
-        Player player = new Player();
         int expectedScore = 0;
 
         player.roll(3);
+
+        assertEquals(expectedScore, player.score());
+    }
+
+    @Test
+    void not_update_the_score_when_it_achieves_a_spare() {
+        int expectedScore = 0;
+
+        player.roll(3);
+        player.roll(7);
 
         assertEquals(expectedScore, player.score());
     }
